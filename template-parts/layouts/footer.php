b9dcc613f5254eb2279f91344999e3ed667eaff4
hailm <hai.lemanh.1310@gmail.com>
2024-11-05 14:26:32 +0700

    feat: update home

--- template-parts/layouts/footer.php
+++ template-parts/layouts/footer.php
@@ -1,3 +1,61 @@
+<?php if (isset($isNav) && $isNav): ?>
+    <nav class="footer__navbar uk-navbar-container uk-navbar uk-position-fixed uk-position-bottom uk-position-z-index-high" uk-navbar>
+        <div class="uk-navbar-left uk-flex-1">
+            <ul class="footer__nav uk-navbar-nav uk-flex-1 uk-child-width-expand uk-grid-match">
+                <li class="footer__nav__li uk-active">
+                    <a href="#" class="footer__nav__a">
+                        <div class="uk-flex uk-flex-column uk-flex-middle">
+                            <div class="footer__nav__bg">
+                                <span class="footer__nav__icon footer__nav__icon--home"></span>
+                            </div>
+                            <div class="uk-navbar-subtitle footer__nav__subtitle">Trang chủ</div>
+                        </div>
+                    </a>
+                </li>
+                <li class="footer__nav__li">
+                    <a href="#" class="footer__nav__a">
+                        <div class="uk-flex uk-flex-column uk-flex-middle">
+                            <div class="footer__nav__bg">
+                                <span class="footer__nav__icon footer__nav__icon--search"></span>
+                            </div>
+                            <div class="uk-navbar-subtitle footer__nav__subtitle">Tìm kiếm</div>
+                        </div>
+                    </a>
+                </li>
+                <li class="footer__nav__li">
+                    <a href="#" class="footer__nav__a">
+                        <div class="uk-flex uk-flex-column uk-flex-middle">
+                            <div class="footer__nav__bg">
+                                <span class="footer__nav__icon footer__nav__icon--store"></span>
+                            </div>
+                            <div class="uk-navbar-subtitle footer__nav__subtitle">Cửa hàng</div>
+                        </div>
+                    </a>
+                </li>
+                <li class="footer__nav__li">
+                    <a href="#" class="footer__nav__a">
+                        <div class="uk-flex uk-flex-column uk-flex-middle">
+                            <div class="footer__nav__bg">
+                                <span class="footer__nav__icon footer__nav__icon--favorite"></span>
+                            </div>
+                            <div class="uk-navbar-subtitle footer__nav__subtitle">Yêu thích</div>
+                        </div>
+                    </a>
+                </li>
+                <li class="footer__nav__li">
+                    <a href="#" class="footer__nav__a">
+                        <div class="uk-flex uk-flex-column uk-flex-middle">
+                            <div class="footer__nav__bg">
+                                <span class="footer__nav__icon footer__nav__icon--account"></span>
+                            </div>
+                            <div class="uk-navbar-subtitle footer__nav__subtitle">Tài khoản</div>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+<?php endif; ?>
 </div>
 <!--/app-->
 </body>
--- template-parts/layouts/header.php
+++ template-parts/layouts/header.php
@@ -1,4 +1,4 @@
-<?php $projectName = ""; ?>
+<?php $projectName = "Thuvienso App"; ?>
 <!DOCTYPE html>
 <html lang="vi">
 <head>
@@ -7,22 +7,144 @@
     <title><?= /** @var TYPE_NAME  */
         (isset($data['title'])) ? $data['title'] : ''; ?> - <?= $projectName ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+
+    <!--Google Fonts-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
     <!--CSS-->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.16.17/dist/css/uikit.min.css" />
+    <link rel="stylesheet" href="dist/css/uikit.min.css" />
     <link rel="stylesheet" href="style.css?v=<?php echo(time()) ?>">
 
     <!--JS-->
-    <script src="https://cdn.jsdelivr.net/npm/@shinsenter/defer.js/dist/defer_plus.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.17/dist/js/uikit.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/uikit@3.16.17/dist/js/uikit-icons.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.21.13/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.21.13/dist/js/uikit-icons.min.js"></script>
     <script src="js/app.js?v=<?php echo(time()) ?>"></script>
+
+    <style>
+        .be-vietnam-pro-100 {
+            font-family: "Be Vietnam Pro", sans-serif;
+            font-weight: 100;
+            font-style: normal;
+        }
+
+        .be-vietnam-pro-200 {
+            font-family: "Be Vietnam Pro", sans-serif;
+            font-weight: 200;
+            font-style: normal;
+        }
+
+        .be-vietnam-pro-300 {
+            font-family: "Be Vietnam Pro", sans-serif;
+            font-weight: 300;
+            font-style: normal;
+        }
+
+        .be-vietnam-pro-400 {
+            font-family: "Be Vietnam Pro", sans-serif;
+            font-weight: 400;
+            font-style: normal;
+        }
+
+        .be-vietnam-pro-500 {
+            font-family: "Be Vietnam Pro", sans-serif;
+            font-weight: 500;
+            font-style: normal;
+        }
+
+        .be-vietnam-pro-600 {
+            font-family: "Be Vietnam Pro", sans-serif;
+            font-weight: 600;
+            font-style: normal;
+        }
+
+        .be-vietnam-pro-700 {
+            font-family: "Be Vietnam Pro", sans-serif;
+            font-weight: 700;
+            font-style: normal;
+        }
+
+        .be-vietnam-pro-800 {
+            font-family: "Be Vietnam Pro", sans-serif;
+            font-weight: 800;
+            font-style: normal;
+        }
+
+        .be-vietnam-pro-900 {
+            font-family: "Be Vietnam Pro", sans-serif;
+            font-weight: 900;
+            font-style: normal;
+        }
+
+        .be-vietnam-pro-100-italic {
+            font-family: "Be Vietnam Pro", sans-serif;
+            font-weight: 100;
+            font-style: italic;
+        }
+
+        .be-vietnam-pro-200-italic {
+            font-family: "Be Vietnam Pro", sans-serif;
+            font-weight: 200;
+            font-style: italic;
+        }
+
+        .be-vietnam-pro-300-italic {
+            font-family: "Be Vietnam Pro", sans-serif;
+            font-weight: 300;
+            font-style: italic;
+        }
+
+        .be-vietnam-pro-400-italic {
+            font-family: "Be Vietnam Pro", sans-serif;
+            font-weight: 400;
+            font-style: italic;
+        }
+
+        .be-vietnam-pro-500-italic {
+            font-family: "Be Vietnam Pro", sans-serif;
+            font-weight: 500;
+            font-style: italic;
+        }
+
+        .be-vietnam-pro-600-italic {
+            font-family: "Be Vietnam Pro", sans-serif;
+            font-weight: 600;
+            font-style: italic;
+        }
+
+        .be-vietnam-pro-700-italic {
+            font-family: "Be Vietnam Pro", sans-serif;
+            font-weight: 700;
+            font-style: italic;
+        }
+
+        .be-vietnam-pro-800-italic {
+            font-family: "Be Vietnam Pro", sans-serif;
+            font-weight: 800;
+            font-style: italic;
+        }
+
+        .be-vietnam-pro-900-italic {
+            font-family: "Be Vietnam Pro", sans-serif;
+            font-weight: 900;
+            font-style: italic;
+        }
+
+    </style>
 </head>
-<body class="">
-<?php require "template-parts/commons/facebook_chat.php"; ?>
+<body class="uk-background-muted">
 <!--app-->
-<div id="app" class="uk-height-viewport uk-offcanvas-content uk-overflow-hidden uk-position-relative">
+<div id="app" style="padding-bottom: 80px" class="uk-background-default width-1024px uk-box-shadow-small uk-margin-auto uk-height-viewport uk-offcanvas-content uk-overflow-hidden uk-position-relative">
+    <nav class="uk-navbar-container" uk-sticky>
+        <div class="uk-container">
+            <div uk-navbar>
+                <div class="uk-navbar-left">
+                    <a class="uk-navbar-item uk-logo" href="#" aria-label="Back to Home"><img src="images/Logo.png" alt=""></a>
+                </div>
+                <div class="uk-navbar-right">
+
+                </div>
+            </div>
+        </div>
+    </nav>
